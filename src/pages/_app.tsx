02fd87d13f755607fa1e6cae4f067ad26a802b23
pdavinunes <p.davi.nunes@gmail.com>
2023-02-20 11:39:49 -0300

    feat: add provider do useShoppingCart

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,31 @@
+import { CartButton } from '@/components/CartButton'
 import { globalStyles } from '@/styles/global'
 import { Container, Header } from '@/styles/pages/app'
 import type { AppProps } from 'next/app'
 import Image from 'next/image'
+import { CartProvider } from 'use-shopping-cart'
 
 import logoImg from '../assets/logo.svg'
 
 globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
-  return (
+  return (  
     <Container>
-      <Header>
-        <Image src={logoImg} alt="" />
-      </Header>
-      <Component {...pageProps} />
+      <CartProvider
+        shouldPersist={true}
+        cartMode="checkout-session"
+        stripe={process.env.STRIPE_PUBLIC_KEY || ''}
+        currency="BRL"
+      >
+        <Header centerlized={pageProps.successPage}>
+        <Image src={logoImg} alt=""/>
+        {
+          !pageProps.successPage &&
+          <CartButton />
+        }
+        </Header>
+        <Component {...pageProps} />
+      </CartProvider>
     </Container>
   )
 }
